5afb9c91ac812dc9ac3b376a03037ae98ba91e5d
Diego Torres Fuerte <dtorresf@redhat.com>
2023-01-31 13:19:47 -0700

    project init

---
+++ src/main/java/com/maincas/maincasapi/MaincasApiApplication.java
@@ -0,0 +1,13 @@
+package com.maincas.maincasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaincasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaincasApiApplication.class, args);
+	}
+
+}
