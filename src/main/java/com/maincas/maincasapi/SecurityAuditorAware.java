15b54bca7633935ba2da0142f7cb993811e88ec8
Diego Torres Fuerte <dtorresf@redhat.com>
2023-02-03 20:45:10 -0700

    incude doc about where to find user information.

--- src/main/java/com/maincas/maincasapi/SecurityAuditorAware.java
+++ src/main/java/com/maincas/maincasapi/SecurityAuditorAware.java
@@ -7,8 +7,11 @@ import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+/**
+ * Attach a user identifier to requests / responses / logs for auditing
+ * User details can be found in the Auth0 Dashboard: https://manage.auth0.com/#/users -> Search the "user_id" by Lucene Syntax
+ */
 public class SecurityAuditorAware implements AuditorAware<String> {
-
   private static final String SYSTEM = "System";
 
   @Override
